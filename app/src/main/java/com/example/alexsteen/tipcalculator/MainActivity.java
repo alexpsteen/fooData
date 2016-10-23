7230632e007ac8fc6991ea9080f88331097e8b65
alexpsteen <alexpsteen@gmail.com>
2016-10-22 20:25:39 -0400

    Creating Home Screen

--- app/src/main/java/com/example/alexsteen/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/alexsteen/tipcalculator/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.alexsteen.tipcalculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * @Author: Alex Steen
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
