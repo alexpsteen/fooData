2a85004a738a0d8db047b6ac4d008eec21d85d2c
alexpsteen <alexpsteen@gmail.com>
2016-11-15 16:36:46 -0500

    Created classes for tabs

--- app/src/main/java/com/example/alexsteen/foodata/Home.java
+++ app/src/main/java/com/example/alexsteen/foodata/Home.java
@@ -117,7 +117,7 @@ public class Home extends AppCompatActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+            View rootView = inflater.inflate(R.layout.tab_calorieCounter, container, false);
             TextView textView = (TextView) rootView.findViewById(R.id.section_label);
             textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
---
+++ app/src/main/java/com/example/alexsteen/foodata/tab_list.java
@@ -0,0 +1,8 @@
+package com.example.alexsteen.foodata;
+
+/**
+ * Created by Alex Steen on 11/15/2016.
+ */
+
+public class tab_list {
+}
